330ca722560f7c157615e7cc18867c9de390cff2
Matthew Yang <matthewyang434@gmail.com>
2023-07-06 20:16:46 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const { stringMatching } = require("expect");
+
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, string) {
+    const result = drivers.filter(drivers => drivers === string || drivers === string.toLowerCase())
+    return result;
+}
+
+function fuzzyMatch(drivers, string) {
+    const result = drivers.filter(drivers => drivers[0] === string[0]);
+    return result;
+}
+
+function matchName(drivers, string) {
+    const result = drivers.filter(drivers => drivers.name === string)
+    return result;
+}
